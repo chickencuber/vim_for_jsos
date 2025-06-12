8ff8045a700836f132561542b34fc8f4d2c05e31
chickencuber <n1batman728@gmail.com>
2025-06-12 15:05:03 -0500

    added thing

--- cmd.exe.js
+++ cmd.exe.js
@@ -1,1 +1,23 @@
-Shell.terminal.add("hello");
+const pre = Shell.terminal.text();
+Shell.terminal.clear();
+if (!args[0]) return "expected 1 argument";
+const path = args[0].toPath();
+let buffer = "";
+
+if (await FS.exists(path)) {
+    if (typeof (await FS.getFromPath(path)) === "object") {
+        return "path can't be a dir";
+    } else {
+        buffer = await FS.getFromPath(path);
+    }
+}
+
+Shell.terminal.add(content);
+
+Shell.keyPressed = (keycode, key) => {
+
+}
+Shell.onExit = () => {
+    Shell.terminal.clear();
+    Shell.terminal.text(pre);
+}
